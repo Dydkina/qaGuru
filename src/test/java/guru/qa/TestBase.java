9ee134d07ff591b1dc7702261929d358b0b14127
sjulia <sjulia@wisebits.com>
2021-11-28 19:22:57 +0300

    added browser capabilities

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -1,16 +1,33 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import guru.qa.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll() { Configuration.startMaximized = true; }
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("name", "Juli S. tests");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;}
 
     @AfterEach
-    void afterEach() { closeWebDriver(); }
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
 }
