c7590b8bb9e8584837578f181f676b4214bc2956
sjulia <sjulia@wisebits.com>
2021-12-10 10:06:39 +0300

    changed credentials

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -1,12 +1,15 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import config.CredentialsConfig;
 import guru.qa.helpers.Attach;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static java.lang.String.format;
 
 public class TestBase {
 
@@ -14,7 +17,14 @@ public class TestBase {
     static void beforeAll() {
         Configuration.startMaximized = true;
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        CredentialsConfig credentials =
+                ConfigFactory.create(CredentialsConfig.class);
+
+        Configuration.remote = format("https://%s:%s@%s",
+                credentials.login(),
+                credentials.password(),
+                credentials.url());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("name", "Juli S. tests");
