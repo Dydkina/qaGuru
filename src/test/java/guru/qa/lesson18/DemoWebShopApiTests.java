d24a1e2cb71ed5c4efa182e4e47c7752a0d0b8bc
sjulia <julisoldatowa@yandex.ru>
2022-03-15 23:47:08 +0300

    rename class

--- src/test/java/guru/qa/lesson18/DemoWebShopApiTests.java
+++ src/test/java/guru/qa/lesson18/DemoWebShopApiTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class demoWebShopApiTests {
+public class DemoWebShopApiTests {
     DemoWebShopPage demoWebShopPage = new DemoWebShopPage();
 
     @BeforeAll
